35c2119fceb8d61531c7ed104888495976862f21
Xin Zou <xinzou@Xins-MBP.fios-router.home>
2018-11-09 11:54:18 -0500

    mdf static root in settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,9 +124,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = '/blog/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/blog/static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
