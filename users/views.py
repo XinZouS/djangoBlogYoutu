9d26a5dd17af5cacbbbac424318538dc49386222
Xin Zou <xinzou@Xins-MBP.fios-router.home>
2018-11-09 17:18:21 -0500

    add save() in user-views.py

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-def register(request, *args, **kwargs):
+def register(request):
 	if request.method == 'POST':
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
